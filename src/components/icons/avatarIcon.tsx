7e295717628f1050652e7721ec5ff48262063ea3
PrissyCorrea <pris.pcd@gmail.com>
2025-07-15 15:14:28 -0300

    feat(icons): atualiza ícones

--- src/components/icons/avatarIcon.tsx
+++ src/components/icons/avatarIcon.tsx
@@ -15,7 +15,7 @@ export function AvatarIcon({
       className={`w-10 h-10 flex items-center justify-center rounded-full ${bgColor}`}
     >
       <span
-        className={`material-symbols-outlined text-white text-xl ${className}`}
+        className={`material-symbols-outlined text-xl ${className}`}
       >
         account_circle
       </span>
--- src/components/icons/trashIcon.tsx
+++ src/components/icons/trashIcon.tsx
@@ -15,7 +15,7 @@ export function TrashIcon({
       className={`w-10 h-10 flex items-center justify-center rounded-full ${bgColor}`}
     >
       <span
-        className={`material-symbols-outlined text-white text-xl ${className}`}
+        className={`material-symbols-outlined text-xl ${className}`}
       >
         delete
       </span>
